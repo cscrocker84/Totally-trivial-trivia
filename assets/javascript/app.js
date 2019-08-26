eece23a39aa7ad15c3549dcae5afb7f3378666ee
Charlie Crocker <cscrocker84@gmail.com>
2019-08-26 10:28:18 -0500

    Got html layout and questions entered into js

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+// Declare Variables
+var correctans = 0;
+var incorrectans = 0;
+var timer = 30;
+var intervalId;
+
+// Create an array of objects with questions and answers
+
+var game = [
+
+    question = {
+        q     : "Which is the most expensive football player?",
+        option1: "Lionel Messi",
+        option2: "Cristiano Ronaldo",
+        option3: "Neymar Jr",
+        option4: "Gareth Bale",
+        Answer: "Cristiano Ronaldo",
+    },
+
+    question = {
+        q     : "Which club has won the most champions league Trophies?",
+        option1: "Real Madrid",
+        option2: "Barcelona FC",
+        option3: "Liverpool FC",
+        option4: "Manchester United",
+        Answer: "Real Madrid",
+    },
+
+    question = {
+        q     : "Who was best FIFA Men's Player for year 2018?",
+        option1: "Lionel Messi",
+        option2: "Cristiano Ronaldo",
+        option3: "Luka Modric",
+        option4: "Mohamed Salah",
+        Answer: "Luka Modric",
+    },
+
+    question = {
+        q     : "Who was best FIFA Men's Coach for year 2018?",
+        option1: "Didier Deschamps",
+        option2: "Zlatko Dalic",
+        option3: "Zinedine Zidane",
+        option4: "Massimiliano Allegri",
+        Answer: "Didier Deschamps",
+    },
+
+    question = {
+        q     : "Who was best FIFA Men's Goalkeeper for year 2018?",
+        option1: "Hugo LLORIS",
+        option2: "Thibaut Courtois",
+        option3: "David De Gea",
+        option4: "Kasper SCHMEICHEL",
+        Answer: "Thibaut Courtois",
+    }
+
+]
